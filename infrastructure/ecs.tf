9479a47ead2fd32bd02e2cae6b88ba2a3661754f
notHyotic <hy0t1c1@gmail.com>
2025-03-25 10:58:57 -0500

    add deploy workflow

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -8,7 +8,8 @@ resource "aws_ecs_service" "main" {
   task_definition = aws_ecs_task_definition.ecs_task.arn
   desired_count   = 1
   launch_type     = "FARGATE" # Or "EC2" if you're using EC2 instances
-
+  force_new_deployment = true
+  
   network_configuration {
     subnets          = [aws_subnet.subnet_a.id]
     security_groups  = [aws_security_group.ecs_sg.id]
