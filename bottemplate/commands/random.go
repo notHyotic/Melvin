33a3b438a52730954f12b1cb0540c66d6b95759e
notHyotic <hy0t1c1@gmail.com>
2025-03-30 09:26:27 -0500

    add random number generator slash command

---
+++ bottemplate/commands/random.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"Melvin/bottemplate"
+	"fmt"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/handler"
+	"math/rand"
+	"time"
+)
+
+var random = discord.SlashCommandCreate{
+	Name:        "random",
+	Description: "generate a random number from 0 to x",
+}
+
+func RandomHandler(b *bottemplate.Bot) handler.CommandHandler {
+	// data := event.SlashCommandInteractionData()
+	roll := generateRandom(0, 100)
+	return func(e *handler.CommandEvent) error {
+		return e.CreateMessage(discord.MessageCreate{
+			Content: fmt.Sprintf("your roll: %d", roll),
+		})
+	}
+}
+
+func generateRandom(min, max int) int {
+	rand.Seed(time.Now().UnixNano()) // Seeds the random number generator
+	return rand.Intn(max-min+1) + min
+}
